87f409d53284d69c8829ea853e257592d299d8d2
Taein142 <xodlsdldy@naver.com>
2024-04-17 00:18:00 +0900

    done applyBookSelect

--- src/main/java/com/rep/book/bookrepboot/controller/TradeController.java
+++ src/main/java/com/rep/book/bookrepboot/controller/TradeController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,20 +25,36 @@ public class TradeController {
     @GetMapping("book-select")
     public String bookSelect(@RequestParam(value = "keyword", required = false) String keyword,
                              @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
-                             Model model) {
+                             int checkNum, Model model) {
         log.info("bookSelect");
 
         String loggedInUserEmail = SecurityUtil.getCurrentUserEmail();
         List<BookDTO> bookList = tradeService.getBookByDB(loggedInUserEmail, keyword);
         List<PageDTO> bookPageList = MainUtil.setPaging(bookList,6);
-        System.out.println(bookPageList);
 
         pageNum = Math.min(pageNum, bookPageList.size());
 
         model.addAttribute("bookList", bookPageList.get(pageNum - 1));
         model.addAttribute("keyword", keyword);
         model.addAttribute("currentPageNum", pageNum);
+        model.addAttribute("checkNum", checkNum);
+        model.addAttribute("bookListSize", bookPageList.size());
 
         return "th/bookSelect";
+    }
+
+    @GetMapping("apply-book-select")
+    public String applyBookSelect(@RequestParam("checkNum") int checkNum, BookDTO bookDTO, RedirectAttributes rttr){
+        log.info("applyBookSelect");
+        rttr.addFlashAttribute("book", bookDTO);
+        log.info("bookDTO {}", bookDTO);
+
+        if (checkNum == 1){
+            return "redirect:/tradeRegistration";
+        } else if (checkNum == 2){
+            return "redirect:/tradeApplication";
+        } else {
+            return "redirect:/shareHouse";
+        }
     }
 }
