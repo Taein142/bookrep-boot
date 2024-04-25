9bfdd50f9d6fa7efd7d0bb6127ab929106c3b799
Taein142 <xodlsdldy@naver.com>
2024-04-25 14:57:13 +0900

    add private at autowired in test method

--- src/test/java/com/rep/book/bookrepboot/BookTradeTest.java
+++ src/test/java/com/rep/book/bookrepboot/BookTradeTest.java
@@ -15,7 +15,7 @@ import java.util.List;
 @SpringBootTest
 public class BookTradeTest {
     @Autowired
-    BookTradeService bookTradeService;
+    private BookTradeService bookTradeService;
 
     @Test
     public void saveTradeResisterTest() {
