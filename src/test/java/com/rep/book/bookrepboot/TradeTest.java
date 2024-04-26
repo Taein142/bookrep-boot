8c645aed72e2b387587b0f43f3089bb783746c87
Taein142 <xodlsdldy@naver.com>
2024-04-26 11:45:53 +0900

    check user trade logic

--- src/test/java/com/rep/book/bookrepboot/TradeTest.java
+++ src/test/java/com/rep/book/bookrepboot/TradeTest.java
@@ -1,10 +1,13 @@
 package com.rep.book.bookrepboot;
 
+import com.rep.book.bookrepboot.dto.MsgDTO;
 import com.rep.book.bookrepboot.dto.PageDTO;
 import com.rep.book.bookrepboot.service.TradeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 import java.util.List;
 
@@ -18,5 +21,33 @@ public class TradeTest {
         String testEmail = "test01@naver.com";
         List<PageDTO> testList = tradeService.getTradeListByEmail(testEmail);
         System.out.println(testList);
+    }
+
+    @Test
+    public void sendTradeMsgTest(){
+        MsgDTO test = new MsgDTO();
+        test.setBook_trade_id(15L);
+        test.setSent_user_email("test02@naver.com");
+        test.setSent_book_isbn("9781506292380");
+        test.setReceived_user_email("test01@naver.com");
+        test.setReceived_book_isbn("9788965795223");
+        RedirectAttributes rttr = new RedirectAttributesModelMap();
+        tradeService.saveTradeMsg(test, rttr);
+        System.out.println(rttr.getFlashAttributes());
+    }
+
+    @Test
+    public void getMsgByIdTest(){
+        Long testId = 1L;
+        MsgDTO test = tradeService.getMsgByID(testId);
+        System.out.println(test);
+    }
+
+    @Test
+    public void updateTradeMsgStatusTest(){
+        Long testId = 4L;
+        String testEmail = "test01@naver.com";
+        boolean testResult = tradeService.updateTradeMsgStatus(testId, testEmail,1);
+        System.out.println(testResult);
     }
 }
