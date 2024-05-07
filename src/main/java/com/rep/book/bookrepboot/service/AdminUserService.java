5005993001e7a70b41fb893d4f5367fbc61e7804
Taein142 <xodlsdldy@naver.com>
2024-05-07 13:11:59 +0900

    update showUserManagement logics & create driverMain.html

--- src/main/java/com/rep/book/bookrepboot/service/AdminUserService.java
+++ src/main/java/com/rep/book/bookrepboot/service/AdminUserService.java
@@ -1,15 +1,22 @@
 package com.rep.book.bookrepboot.service;
 
 import com.rep.book.bookrepboot.dao.UserDao;
+import com.rep.book.bookrepboot.dto.PageDTO;
+import com.rep.book.bookrepboot.dto.UserDTO;
+import com.rep.book.bookrepboot.util.MainUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Slf4j
 public class AdminUserService {
     @Autowired
     private UserDao userDao;
+
     public boolean deleteUserByEmail(String userEmail) {
         log.info("deleteUserByEmail()");
         boolean result = false;
@@ -22,5 +29,19 @@ public class AdminUserService {
             result = false;
         }
         return result;
+    }
+
+    public List<PageDTO> getUserByKeyword(String option, String keyword) {
+        log.info("getUserByKeyword()");
+        List<UserDTO> userList = new ArrayList<>();
+
+        if (option.equals("email")){
+            userList = userDao.getUserListByEmail(keyword);
+        } else if (option.equals("name")) {
+            userList = userDao.getUserListByName(keyword);
+        } else {
+            userList = null;
+        }
+        return MainUtil.setPaging(userList, 10);
     }
 }
